fbb5f96b8d4858bafd41c884af09968b9a107284
Stefan <rozov200@gmail.com>
2026-03-23 15:15:53 +0300

    тесты для is passed

--- src/GradeCalculator.py
+++ src/GradeCalculator.py
@@ -1,3 +1,5 @@
+
+
 class GradeCalculator:
     def __init__(self, points):
         self.points = points
@@ -5,41 +7,48 @@ class GradeCalculator:
         self.points_list = []
 
     def getGrade(self, points):
-        if not isinstance(points, (int, float)):
-            raise ValueError("points должен быть числом")
-
-        if points < 0 or points > 100:
-            raise ValueError("Оценка должна быть от 0 до 100")
-        self.points = points
-        if self.points < 0 or self.points > 100:
-            return ValueError
-        elif self.points <=44:
-            return 'F'
-        elif self.points <=59:
-            return 'D'
-        elif self.points <=74:
-            return 'C'
-        elif self.points <=89:
-            return 'B'
-        else:
-            return 'A'
-
-    def is_passed(self):
         try:
-            if self.points > 45:
-                return True
+            if points < 0 or points > 100:
+                raise ValueError
+            self.points = points
+            if self.points < 0 or self.points > 100:
+                return ValueError
+            elif self.points <= 44:
+                return 'F'
+            elif self.points <= 59:
+                return 'D'
+            elif self.points <= 74:
+                return 'C'
+            elif self.points <= 89:
+                return 'B'
             else:
+                return 'A'
+        except ValueError:
+            return ValueError
+        except TypeError:
+            return TypeError
+
+    def is_passed(self, points):
+        try:
+            self.points = points
+            if self.points > 45 and 101 > self.points:
+                return True
+            elif self.points >= 0 and self.points <= 45:
                 return False
         except ValueError:
-            return ValueError
+            if self.points < 0 or self.points > 100:
+                return ValueError
+        except TypeError:
+            return TypeError
 
-    def get_average(self, points_list) -> float:
+    def get_average(self, points_list):
         try:
             self.points_list = points_list
-            return sum(self.points_list) / len(points_list)
+            result = sum(self.points_list) / len(self.points_list)
+            return float(result)
         except ValueError:
-            if points_list <0 or points_list > 100:
+            if points_list < 0 or points_list > 100:
                 return ValueError
-
-
+        except TypeError:
+            return TypeError
 
--- tests/test_GradeCalculator.py
+++ tests/test_GradeCalculator.py
@@ -12,8 +12,27 @@ def test_getGrade_0_returns_F(gradeCalculator):
 
 def test_getGrade_minus1_returns_ValueError(gradeCalculator):
     result = gradeCalculator.getGrade(-1)
-    assert result == GradeCalculator.ValueError
+    assert result == ValueError
 
 def test_getGrade_ab_returns_ValueError(gradeCalculator):
     result = gradeCalculator.getGrade('ab')
-    assert result == GradeCalculator.ValueError
+    assert result == TypeError
+
+def test_IsPassed_100_returns_True(gradeCalculator):
+    result = gradeCalculator.is_passed(100)
+    assert result == True
+
+def test_IsPassed_45_returns_False(gradeCalculator):
+    result = gradeCalculator.is_passed(45)
+    assert result == False
+
+def test_IsPassed_minus1_returns_ValueError(gradeCalculator):
+    with pytest.raises(ValueError) as exc_info:
+        gradeCalculator.is_passed(-1)
+        exc_info == ValueError
+
+
+def test_IsPassed_ab_returns_TypeError(gradeCalculator):
+    result = gradeCalculator.is_passed('ab')
+    assert result == TypeError
+
