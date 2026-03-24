b81fb8395b96f1a65551dfcf5859fcefb73b4761
Stefan <rozov200@gmail.com>
2026-03-24 16:27:02 +0300

    тесты для get_average

--- src/GradeCalculator.py
+++ src/GradeCalculator.py
@@ -29,26 +29,27 @@ class GradeCalculator:
             return TypeError
 
     def is_passed(self, points):
+        self.points = points
         try:
-            self.points = points
-            if self.points > 45 and 101 > self.points:
+            if not (0 <= points <= 100):
+                raise ValueError("Points must be a number between 0 and 100")
+            elif self.points > 45 and 101 > self.points:
                 return True
             elif self.points >= 0 and self.points <= 45:
                 return False
-        except ValueError:
-            if self.points < 0 or self.points > 100:
-                return ValueError
         except TypeError:
             return TypeError
+
 
     def get_average(self, points_list):
         try:
             self.points_list = points_list
+            if not points_list:
+                raise ZeroDivisionError("Points list cannot be empty")
+            elif any(p < 0 for p in points_list):
+                raise ValueError("Points cannot be negative")
             result = sum(self.points_list) / len(self.points_list)
             return float(result)
-        except ValueError:
-            if points_list < 0 or points_list > 100:
-                return ValueError
         except TypeError:
             return TypeError
 
--- tests/test_GradeCalculator.py
+++ tests/test_GradeCalculator.py
@@ -29,10 +29,35 @@ def test_IsPassed_45_returns_False(gradeCalculator):
 def test_IsPassed_minus1_returns_ValueError(gradeCalculator):
     with pytest.raises(ValueError) as exc_info:
         gradeCalculator.is_passed(-1)
-        exc_info == ValueError
-
+    assert str(exc_info.value) == "Points must be a number between 0 and 100"
 
 def test_IsPassed_ab_returns_TypeError(gradeCalculator):
     result = gradeCalculator.is_passed('ab')
     assert result == TypeError
 
+def test_get_average_67_and_42_returns_54and5(gradeCalculator, grades_factory):
+    points_list = grades_factory(67, 42)
+    result = gradeCalculator.get_average(points_list)
+    assert result == 54.5
+
+def test_get_average_67_returns_67(gradeCalculator, grades_factory):
+    points_list = grades_factory(67)
+    result = gradeCalculator.get_average(points_list)
+    assert result == 67
+
+def test_get_average_none_returns_ZeroDivisionError(gradeCalculator, grades_factory):
+    points_list = grades_factory()
+    with pytest.raises(ZeroDivisionError) as exc_info:
+        gradeCalculator.get_average(points_list)
+    assert str(exc_info.value) == "Points list cannot be empty"
+
+def test_get_average_minus1_and_zero_returns_ValueError(gradeCalculator, grades_factory):
+    points_list = grades_factory(-1, 0)
+    with pytest.raises(ValueError) as exc_info:
+        gradeCalculator.get_average(points_list)
+    assert str(exc_info.value) == "Points cannot be negative"
+
+def test_get_average_ab_and_zero_returns_TypeError(gradeCalculator, grades_factory):
+    points_list = grades_factory('ab', 0)
+    result = gradeCalculator.get_average(points_list)
+    assert result == TypeError
